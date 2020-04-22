246a2d948e2aa47872d80e44820d3beb543a3afb
Deepali yadav <dy19648@gmail.com>
2020-04-22 16:19:19 +0530

    Sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Deepali@19'
+const sequelize = new Sequelize('node-complete', 'root', 'Deepali@19', {
+    dialect: 'mysql', 
+    host: 'localhost'
 });
 
-module.exports = pool.promise(); 
+module.exports = sequelize;
