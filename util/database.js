453583ea2fc70835deecc3410f55dd382306eb09
Deepali yadav <dy19648@gmail.com>
2020-04-24 21:57:59 +0530

    MongoDB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,33 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'Deepali@19', {
-    dialect: 'mysql', 
-    host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://max:3y8EdfdpGsiqJWZn@cluster0-1ya5e.mongodb.net/shop?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    }
+)
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
