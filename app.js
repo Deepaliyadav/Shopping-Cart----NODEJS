fde853a7b155221391431aabf612b9d44f4f8a5d
Deepali yadav <dy19648@gmail.com>
2020-05-08 14:20:25 +0530

    Sessions

--- app.js
+++ app.js
@@ -3,38 +3,62 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://max:3y8EdfdpGsiqJWZn@cluster0-1ya5e.mongodb.net/shop?retryWrites=true&w=majority';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById('5ea7074ab2e6221ce0ced3c7')
+  if(!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
     })
     .catch(err => console.log(err));
-});
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://max:3y8EdfdpGsiqJWZn@cluster0-1ya5e.mongodb.net/shop?retryWrites=true&w=majority',
-                    { useNewUrlParser: true,
-                      useUnifiedTopology: true }  ) 
+mongoose.connect(MONGODB_URI,{ 
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }, () =>{
+      console.log("we are connected to COMPASS")
+    })
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
